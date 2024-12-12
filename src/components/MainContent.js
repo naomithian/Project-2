a148cb2cb24f9d374b1be6b4b3746a478c063367
Naomi Thian <nc090290@gmail.com>
2024-12-12 15:49:56 -0500

    Add new text under Who We Are in MainContent component

---
+++ src/components/MainContent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './MainContent.css';
+import whoWeAreImg from '../assets/WhoWeAre.png';
+import elementImg from '../assets/Element.png'; 
+
+const MainContent = () => {
+  return (
+    <div className="new-layout">
+      <div className="image-left">
+        <img src={whoWeAreImg} alt="Left side image" />
+      </div>
+      <div className="image-center">
+        <img src={elementImg} alt="Center image" />
+      </div>
+      <div className="text-right">
+        <h1>Who We Are</h1>
+        <p>NDH is more than design—it's emotion. Marrying luxury with meaning, we shape spaces that mirror individual narratives and foster connections. Each room becomes an echo of personal stories and dreams realized.</p>
+        <p id="ndh"></p>
+        <a href="#" className="read-more">READ MORE</a>
+      </div>
+    </div>
+  );
+};
+
+export default MainContent;
