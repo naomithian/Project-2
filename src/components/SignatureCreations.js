2d5585f140a44ef3caa5727ffb9605ff064cffc6
Naomi Thian <nc090290@gmail.com>
2024-12-12 16:05:16 -0500

    Add new text under each creation in SignatureCreations component

--- src/components/SignatureCreations.js
+++ src/components/SignatureCreations.js
@@ -12,6 +12,7 @@ const SignatureCreations = () => {
         <img src={oceanicImg} alt="Oceanic Elegance" />
         <div className="creation-description">
           <h3>Oceanic Elegance</h3>
+          <p>Embrace the tranquility of coastal living with oceanic elements.</p>
           <p id="embrace"></p>
           <a href="#" className="learn-more">LEARN MORE</a>
         </div>
@@ -20,6 +21,7 @@ const SignatureCreations = () => {
         <img src={urbanImg} alt="Urban Sophistication" />
         <div className="creation-description">
           <h3>Urban Sophistication</h3>
+          <p>Modern and minimalist design in a contemporary urban setting.</p>
           <p id="modern"></p>
           <a href="#" className="learn-more">LEARN MORE</a>
         </div>
@@ -28,6 +30,7 @@ const SignatureCreations = () => {
         <img src={rusticImg} alt="Rustic Revival" />
         <div className="creation-description">
           <h3>Rustic Revival</h3>
+          <p>The charm of a country house with raw, earthy aesthetics.</p>
           <p id="charm"></p>
           <a href="#" className="learn-more">LEARN MORE</a>
         </div>
